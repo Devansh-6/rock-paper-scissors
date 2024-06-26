4a4d07dd526af5537268e1c8fa2fe0d25a5e829f
Devansh-6 <66192466+Devansh-6@users.noreply.github.com>
2024-06-25 20:41:13 -0400

    Implement basic rock logic

--- script.js
+++ script.js
@@ -2,27 +2,41 @@ function getComputerChoice(){
     let randInt = Math.trunc(Math.random() * 3) + 1;
     
     if (randInt == 1){
-        return "Rock";
+        return "rock";
     } else if (randInt == 2){
-        return "Paper";
+        return "paper";
     } else if (randInt == 3){
-        return "Scissors";
+        return "scissors";
     }
     
 }
 
 function getHumanChoice(){
-    return prompt("What is your choice?");
+    return prompt("What is your choice?").toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice){
-
+    if (humanChoice == "rock"){
+        if (computerChoice == "rock"){
+            console.log("You tie! Rock ties rock.");
+        } else if (computerChoice == "paper"){
+            console.log("You lose! Paper beats rock.");
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats scissors.");
+            humanScore++;
+        }
+    }
 }
 
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+// console.log(getComputerChoice());
+// console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
