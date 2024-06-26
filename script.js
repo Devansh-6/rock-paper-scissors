a634ce574389565c0b959ebe52c0040b770a9ff6
Devansh-6 <66192466+Devansh-6@users.noreply.github.com>
2024-06-25 20:32:27 -0400

    Add getComputerChoice and getHumanChoice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+function getComputerChoice(){
+    let randInt = Math.trunc(Math.random() * 3) + 1;
+    
+    if (randInt == 1){
+        return "Rock";
+    } else if (randInt == 2){
+        return "Paper";
+    } else if (randInt == 3){
+        return "Scissors";
+    }
+    
+}
+
+function getHumanChoice(){
+    let humanChoice = prompt("What is your choice?");
+    return humanChoice;
+}
+
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
